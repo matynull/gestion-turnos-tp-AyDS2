bacf2d073d42b51262cd7f29bb5b50397473bcf3
Tentaculo-kun <lucasunmdp@gmail.com>
2021-04-30 18:14:46 -0300

    vista empleado

---
+++ src/paquete/vista/VentanaEmpleado.java
@@ -0,0 +1,65 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.border.TitledBorder;
+
+public class VentanaEmpleado extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaEmpleado frame = new VentanaEmpleado();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaEmpleado() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new TitledBorder(null, "Empleado", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(2, 2, 0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		
+		JLabel LabelEmpleado = new JLabel("Empleado");
+		panel.add(LabelEmpleado);
+		
+		JPanel panel_2 = new JPanel();
+		contentPane.add(panel_2);
+		
+		JLabel LabelBox = new JLabel("Box");
+		panel_2.add(LabelBox);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1);
+		
+		JLabel LabelVacio = new JLabel("");
+		panel_1.add(LabelVacio);
+		
+		JButton BotonTomarTurno = new JButton("Tomar Turno");
+		contentPane.add(BotonTomarTurno);
+	}
+
+}
